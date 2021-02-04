a8d789bb0795ede76e2e92056bf1e769f2558b26
Cerem Cem ASLAN <cem@aktos.io>
2021-02-04 17:29:05 +0300

    added server and laptop profiles

--- install.sh
+++ install.sh
@@ -3,14 +3,89 @@ set -eu -o pipefail
 safe_source () { [[ ! -z ${1:-} ]] && source $1; _dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"; _sdir=$(dirname "$(readlink -f "$0")"); }; safe_source
 # end of bash boilerplate
 
+
+show_help(){
+    cat <<HELP
+
+    $(basename $0) [options] --for-server|--for-laptop
+
+    Options:
+
+        --for-laptop      : Mark system state dirty on every PERIOD. 
+        --for-server      : Run Btrfs scrub on every PERIOD
+        --period          : systemd service period. Default: "weekly"
+
+HELP
+}
+
+die(){
+    >&2 echo
+    >&2 echo "$@"
+    exit 1
+}
+
+help_die(){
+    >&2 echo
+    >&2 echo "$@"
+    show_help
+    exit 1
+}
+
+# Parse command line arguments
+# ---------------------------
+# Initialize parameters
+exe_switch=
+service_type=forking
+PERIOD="weekly"  # "*-*-* 22:30:00"
+# ---------------------------
+args_backup=("$@")
+args=()
+_count=1
+while [ $# -gt 0 ]; do
+    key="${1:-}"
+    case $key in
+        -h|-\?|--help|'')
+            show_help    # Display a usage synopsis.
+            exit
+            ;;
+        # --------------------------------------------------------
+        --for-laptop)
+            exe_switch="--mark"
+            ;;
+        --for-server)
+            exe_switch="--start"
+            service_type=idle      # like "simple", but wait till idle
+            ;;
+        --period) shift
+            PERIOD=$1
+            ;;
+        # --------------------------------------------------------
+        -*) # Handle unrecognized options
+            help_die "Unknown option: $1"
+            ;;
+        *)  # Generate the new positional arguments: $arg1, $arg2, ... and ${args[@]}
+            if [[ ! -z ${1:-} ]]; then
+                declare arg$((_count++))="$1"
+                args+=("$1")
+            fi
+            ;;
+    esac
+    [[ -z ${1:-} ]] && break || shift
+done; set -- "${args_backup[@]}"
+# Use $arg1 in place of $1, $arg2 in place of $2 and so on, 
+# "$@" is in the original state,
+# use ${args[@]} for new positional arguments  
+
 [[ $(whoami) = "root" ]] || { sudo $0 "$@"; exit 0; }
 
-echo "Installing dependencies if missing."
-hash curl 2> /dev/null || apt-get install curl
-echo "Dependencies installed."
+if ! hash curl 2> /dev/null; then 
+    echo "Installing dependencies if missing."
+    apt-get install curl
+    echo "Dependencies installed."
+fi
 
-PERIOD="weekly"
-#PERIOD="*-*-* 22:30:00"
+[[ -z $exe_switch ]] && help_die "Profile is required."
+
 EXECUTABLE_PATH="$_sdir/scrub-mounted.sh"
 
 SERVICE_NAME="btrfs-scrub-mounted"
@@ -24,8 +99,8 @@ Description=Run 'btrfs scrub' on all mounted disks.
 
 [Service]
 User=root
-Type=forking
-ExecStart=$EXECUTABLE_PATH --mark
+Type=$service_type
+ExecStart=$EXECUTABLE_PATH $exe_switch
 
 SERVICE
 
@@ -33,7 +108,7 @@ timer_file="$SERVICE_PATH/$SERVICE_NAME.timer"
 echo "installing $timer_file"
 cat << TIMER > "$timer_file"
 [Unit]
-Description=Runs btrfs scrub on all discs $PERIOD
+Description=Run 'btrfs scrub' on all mounted disks.
 
 [Timer]
 OnCalendar=$PERIOD
--- scrub-mounted.sh
+++ scrub-mounted.sh
@@ -8,6 +8,35 @@
 set -eu
 safe_source () { [[ ! -z ${1:-} ]] && source $1; _dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"; _sdir=$(dirname "$(readlink -f "$0")"); }; safe_source
 
+show_help(){
+    cat <<HELP
+
+    Resumes any interrupted scrub operations on all mounted Btrfs filesystems.
+
+    $(basename $0) [options]
+
+    Options:
+
+        --mark      : Only mark mounted Btrfs filesystems as dirty, exit immediately.
+        --start     : Start scrubbing on all filesystems and wait till finish.
+
+HELP
+}
+
+die(){
+    >&2 echo
+    >&2 echo "$@"
+    exit 1
+}
+
+help_die(){
+    >&2 echo
+    >&2 echo "$@"
+    show_help
+    exit 1
+}
+
+
 [[ $(whoami) = "root" ]] || { sudo $0 "$@"; exit 0; }
 
 TMP_OUTPUT="/tmp/btrfs-scrub.out"
@@ -23,15 +52,36 @@ _kill(){
 }
 
 scrub_status(){
+    local fs=$1
     btrfs scrub status $fs | grep -E "^Status" | awk -F: '{print $2}' | tr -d ' '
 }
 
+
+scrub_start(){
+    local fs=$1
+    local curr=$(scrub_status $fs)
+    if [[ "$curr" != "running" ]]; then
+        echostamp "Starting scrub task for $fs"
+        btrfs scrub start -Bd $fs && \
+            btrfs scrub status -d $fs >> $TMP_OUTPUT  &
+    else
+        echostamp "Scrub job is already running for $fs. Skipping."
+    fi
+}
+
 scrub_mark_dirty(){
-    btrfs scrub start $fs
-    while sleep 1; do
-        [[ $(scrub_status $fs) == "running" ]] && break
-    done
-    btrfs scrub cancel $fs
+    local fs=$1
+    if [[ "$(scrub_status $fs)" == "" ]]; then 
+        # clean state 
+        echostamp "Marking $fs as dirty."
+        btrfs scrub start $fs
+        while sleep 1; do
+            [[ $(scrub_status $fs) == "running" ]] && break
+        done
+        btrfs scrub cancel $fs
+    else
+        echostamp "State of $fs is already non-clean. Skipping."
+    fi
 }
 
 scrub_resume(){
@@ -48,28 +98,70 @@ scrub_resume(){
     fi
 }
 
-[[ "${1:-}" == "--mark" ]] && trigger_new=true || trigger_new=false
+
+# Parse command line arguments
+# ---------------------------
+# Initialize parameters
+only_mark=false
+do_run=false
+# ---------------------------
+args_backup=("$@")
+args=()
+_count=1
+while [ $# -gt 0 ]; do
+    key="${1:-}"
+    case $key in
+        -h|-\?|--help|'')
+            show_help    # Display a usage synopsis.
+            exit
+            ;;
+        # --------------------------------------------------------
+        --mark) 
+            only_mark=true
+            ;;
+        --start)
+            do_run=true
+            ;;
+        # --------------------------------------------------------
+        -*) # Handle unrecognized options
+            help_die "Unknown option: $1"
+            ;;
+        *)  # Generate the new positional arguments: $arg1, $arg2, ... and ${args[@]}
+            if [[ ! -z ${1:-} ]]; then
+                declare arg$((_count++))="$1"
+                args+=("$1")
+            fi
+            ;;
+    esac
+    [[ -z ${1:-} ]] && break || shift
+done; set -- "${args_backup[@]}"
+# Use $arg1 in place of $1, $arg2 in place of $2 and so on, 
+# "$@" is in the original state,
+# use ${args[@]} for new positional arguments  
 
 # Process all apparent devices
 while read fs; do
-    if $trigger_new; then
-        echostamp "Marking $fs as dirty."
+    if $only_mark; then
         scrub_mark_dirty $fs
-    else
+    elif $do_run; then
+        scrub_start $fs
+    else # resume 
         scrub_resume $fs
     fi
 done < <( cat /proc/mounts | awk '$3 == "btrfs" {print $1}' | uniq )
 
-if $trigger_new; then
+if $only_mark; then
     echostamp "All available devices are marked as dirty. Exiting."
     exit 0
 fi
 
-# cancel all running scrubs on interrupt
-trap _kill EXIT
+if ! $do_run; then
+    # cancel all running scrubs on interrupt
+    trap _kill EXIT
+fi
 
 wait # for any scrub operation
-echostamp "All scrub operations are completed."
+echostamp "Done."
 
 scrub_error_status(){
     local result=$1
